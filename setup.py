e50abdb903f5632fca7eb7aad374f563ce67d864
doronz <doron88@gmail.com>
2023-08-09 18:48:30 +0300

    modernize package build

--- setup.py
+++ setup.py
@@ -7,25 +7,4 @@ if sys.platform == 'darwin':
 else:
     sources = ['linux_pytun.c']
 
-setup(name='pytun-pmd3',
-      author='doronz88',
-      author_email='doron88@gmail.com',
-      maintainer='doronz88',
-      maintainer_email='doron88@gmail.com',
-      url='https://github.com/doronz88/pytun-pmd3',
-      description='Linux & Darwin TUN/TAP wrapper for Python',
-      long_description_content_type='text/markdown',
-      long_description=open('README.md').read(),
-      version='0.0.1',
-      ext_modules=[Extension('pytun_pmd3', sources)],
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: POSIX :: Linux',
-          'Operating System :: MacOS',
-          'Programming Language :: C',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 3',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-          'Topic :: System :: Networking'])
+setup(name='pytun-pmd3', ext_modules=[Extension('pytun_pmd3', sources, include_dirs=['.'])])
